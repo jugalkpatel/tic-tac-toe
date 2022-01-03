15f333174dc13e4d6ccec7523fd2699fa73cdbee
jugalkpatel <ptjimmy99@gmail.com>
2022-01-03 16:54:49 +0530

    style: add spaces and quotes

--- main.js
+++ main.js
@@ -3,4 +3,5 @@ import { createPlayBoard } from "./scripts/createPlayBoard";
 
 const app = document.querySelector("#app");
 const gameBoard = createPlayBoard();
+
 app.appendChild(gameBoard);
--- scripts/constants.js
+++ scripts/constants.js
@@ -1,6 +1,6 @@
 export const NO_OF_BOXES_IN_PLAYBOARD = 9;
 export const MEDIA_QUERIES = {
-    "small": window.matchMedia('(min-width: 568px'),
-    "medium": window.matchMedia('(min-width: 768px)'),
-    "large": window.matchMedia('(min-width: 992px)'),
-} 
+  small: window.matchMedia("(min-width: 568px"),
+  medium: window.matchMedia("(min-width: 768px)"),
+  large: window.matchMedia("(min-width: 992px)"),
+};
