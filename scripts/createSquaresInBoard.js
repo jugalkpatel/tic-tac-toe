b7261d29c5948bb60ad43fea0b6c839eb752a8a9
jugalkpatel <ptjimmy99@gmail.com>
2022-01-03 23:42:20 +0530

    refactor: change play-board squares element from section to button

--- scripts/createSquaresInBoard.js
+++ scripts/createSquaresInBoard.js
@@ -7,14 +7,18 @@ export function createSquaresInBoard() {
   let currentValue = values[0];
 
   for (let i = 0; i < NO_OF_BOXES_IN_PLAYBOARD; i++) {
-    const box = document.createElement("section");
+    const box = document.createElement("button");
+    box.id = `pv__sq-${i}`;
+
+    box.style.backgroundColor = "transparent";
     box.style.border = "1px solid black";
     box.style.display = "flex";
+    box.style.color = "black";
     box.style.justifyContent = "center";
     box.style.alignItems = "center";
     box.style.fontSize = "1.5rem";
 
-    box.addEventListener("click", () => {
+    function clickHandler() {
       box.textContent = currentValue;
 
       if (currentValue === values[0]) {
@@ -22,7 +26,11 @@ export function createSquaresInBoard() {
       } else {
         currentValue = values[0];
       }
-    });
+
+      box.disabled = true;
+    }
+
+    box.addEventListener("click", clickHandler);
 
     squares.push(box);
   }
