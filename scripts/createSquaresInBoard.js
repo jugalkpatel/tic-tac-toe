2805266952e680b2a4e2261dfaa1e74b8466689c
jugalkpatel <ptjimmy99@gmail.com>
2022-01-03 17:02:17 +0530

    feat: add Noughts and Crosses squares in the play board

--- scripts/createSquaresInBoard.js
+++ scripts/createSquaresInBoard.js
@@ -2,9 +2,28 @@ import { NO_OF_BOXES_IN_PLAYBOARD } from "./constants";
 export function createSquaresInBoard() {
   const squares = [];
 
+  const values = ["O", "X"];
+
+  let currentValue = values[0];
+
   for (let i = 0; i < NO_OF_BOXES_IN_PLAYBOARD; i++) {
     const box = document.createElement("section");
     box.style.border = "1px solid black";
+    box.style.display = "flex";
+    box.style.justifyContent = "center";
+    box.style.alignItems = "center";
+    box.style.fontSize = "1.5rem";
+
+    box.addEventListener("click", () => {
+      box.textContent = currentValue;
+
+      if (currentValue === values[0]) {
+        currentValue = values[1];
+      } else {
+        currentValue = values[0];
+      }
+    });
+
     squares.push(box);
   }
 
