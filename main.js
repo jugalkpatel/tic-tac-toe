827cd989954ad33e170e7fcb6bbc6adf2cd88be7
jugalkpatel <ptjimmy99@gmail.com>
2022-01-05 23:17:11 +0530

    feat: add record-board to the app

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 import "./style.css";
 import { createPlayBoard } from "./scripts/createPlayBoard";
+import { createRecordBoard } from "./scripts/createRecordBoard";
 
 const app = document.querySelector("#app");
-const gameBoard = createPlayBoard();
+const { recordBoard, updateLabel } = createRecordBoard();
+const gameBoard = createPlayBoard(updateLabel);
 
 app.appendChild(gameBoard);
+app.appendChild(recordBoard);
