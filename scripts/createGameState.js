288afa8d47278fc0bb2b9b91e1bde5464f7adc28
jugalkpatel <ptjimmy99@gmail.com>
2022-01-05 17:28:16 +0530

    feat: add state object to createGameState function

--- scripts/createGameState.js
+++ scripts/createGameState.js
@@ -1,14 +1,36 @@
 import { SIZE_OF_THE_BOARD } from "./constants";
+import { createTwoDimensionalArray } from "./utils/createTwoDimensionalArray";
+
 export function createGameState() {
-  const state = new Array(SIZE_OF_THE_BOARD);
+  const initialValue = createTwoDimensionalArray(
+    SIZE_OF_THE_BOARD,
+    SIZE_OF_THE_BOARD
+  );
 
-  for (let i = 0; i < SIZE_OF_THE_BOARD; i++) {
-    state[i] = new Array(SIZE_OF_THE_BOARD);
-  }
+  const gameState = {
+    value: initialValue,
+    isGameOver: function () {
+      for (let i = 0; i < SIZE_OF_THE_BOARD; i++) {
+        for (let j = 0; j < SIZE_OF_THE_BOARD; j++) {
+          if (this.value[i][j] === "") {
+            return false;
+          }
+        }
+      }
+      return true;
+    },
+    setGameState: function (row, column, value) {
+      console.log(this.value);
+      this.value[row][column] = value;
+    },
+    resetState: function () {
+      for (let i = 0; i < SIZE_OF_THE_BOARD; i++) {
+        for (let j = 0; j < SIZE_OF_THE_BOARD; j++) {
+          this.value[i][j] = "";
+        }
+      }
+    },
+  };
 
-  function setState(row, column, value) {
-    state[row][column] = value;
-  }
-
-  return { state, setState };
+  return gameState;
 }
